6b5630743c808ccfef4f799c644f4194a22f208c
Mahdi Merhi <mahdihassan1@hotmail.com>
2025-03-25 22:16:38 +0200

    update start destination and build.gradle

--- navigation/build.gradle.kts
+++ navigation/build.gradle.kts
@@ -1,27 +1,28 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-tasks.matching { it.name == "kotlinNpmInstall" }.configureEach { enabled = false }
-val isRunningOnJitPack = System.getenv("JITPACK") == "true"
-
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     id("convention.publication")
+    id("com.vanniktech.maven.publish") version "0.31.0-rc2"
+    signing
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
 
 group = "com.blank.moenav"
-version = "1.0.0"
+version = "1.1.0"
 
 kotlin {
     @OptIn(ExperimentalWasmDsl::class)
-    if (isRunningOnJitPack) {
-        jvm()
-    } else {
-        wasmJs {
-            browser()
-            binaries.library()
-        }
+    wasmJs {
+        browser()
+        binaries.library()
     }
 
     sourceSets {
@@ -35,5 +36,57 @@ kotlin {
         }
 
     }
+}
 
+mavenPublishing {
+    tasks.withType<Jar>().configureEach {
+        if (name == "javadocJar") {
+            archiveClassifier.set("javadoc")
+        }
+    }
+
+    coordinates(
+        groupId = "io.blankonline.moe",
+        artifactId = "MoeNav",
+        version = "1.1.0"
+    )
+
+    pom {
+        name.set("MoeNav")
+        description.set("MoeNav is a lightweight navigation library for Jetpack Compose for Web (WASM). It provides URL-based navigation, deep linking, nested navigation graphs, and browser history support, making Compose Web development more seamless.")
+        inceptionYear.set("2025")
+        url.set("https://github.com/MohammadNasrallahBlank/MoeNav")
+
+        licenses {
+            license {
+                name.set("MIT")
+                url.set("https://github.com/MohammadNasrallahBlank/MoeNav/blob/main/LICENSE")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("MohammadNasrallahBlank")
+                name.set("Mohamad Nasrallah")
+                email.set("Moe@blankonline.io")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/MohammadNasrallahBlank/MoeNav")
+        }
+    }
+
+    afterEvaluate {
+        tasks.withType<Jar>().configureEach {
+            if (archiveClassifier.getOrElse("") == "javadoc") {
+                enabled = false
+            }
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
--- sample/composeApp/src/wasmJsMain/kotlin/sample/app/main.kt
+++ sample/composeApp/src/wasmJsMain/kotlin/sample/app/main.kt
@@ -11,7 +11,7 @@ fun main() {
 
     val navController = MoeNavController.createWithBrowserHistory(
         routes = SampleRoutes.routes,
-        startDestination = SampleRoutes.About
+        startDestination = SampleRoutes.Home
     )
 
     val body: HTMLElement = document.body ?: return
